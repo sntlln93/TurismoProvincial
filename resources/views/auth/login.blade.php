8fe6b705a0f45475092aaec8c53ef16aa1c59008
Matias Santillan <sntlln.93@gmail.com>
2021-04-08 20:25:00 -0300

    now only numbers are allowed in login's dni-input

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,35 +7,47 @@
             margin: 0 0 0 1em;
             font-weight: 500;
         }
+
+        /* Chrome, Safari, Edge, Opera */
+        input::-webkit-inner-spin-button {
+            -webkit-appearance: none;
+            margin: 0;
+        }
+
+        /* Firefox */
+        input[type=number] {
+            -moz-appearance: textfield;
+        }
+
     </style>
 @endsection
 
 @section('content')
-<div class="login-user"> 
-    <h2>INICIAR SESIÓN</h2>
-    
-    @if($errors->any())
-        @foreach ($errors->all() as $error)
-        <small class="error-message">Nombre de usuario o contraseña incorrectos.</small>
-        @endforeach
-    @endif
-    
-    <form action="{{ url('login') }}" method="POST" autocomplete="off">
-        @csrf
+    <div class="login-user">
+        <h2>INICIAR SESIÓN</h2>
 
-        <input style="display:none" ariahidden="true">
-        <input type="password" style="display:none" ariahidden="true">
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <small class="error-message">Nombre de usuario o contraseña incorrectos.</small>
+            @endforeach
+        @endif
 
-        <div class="input-group">
-            <i class="icon-user"></i>
-            <input type="text" name="dni" placeholder="DNI">
-        </div>
-        <div class="input-group">
-            <i class="icon-key-1"></i>
-            <input type="password" name="password" placeholder="Contraseña">
-        </div>
-        <button type="submit" class="btn-login">Entrar</button>
-        
-    </form>
-</div>
+        <form action="{{ url('login') }}" method="POST" autocomplete="off">
+            @csrf
+
+            <input style="display:none" ariahidden="true">
+            <input type="password" style="display:none" ariahidden="true">
+
+            <div class="input-group">
+                <i class="icon-user"></i>
+                <input class="dni-input" type="number" name="dni" placeholder="DNI">
+            </div>
+            <div class="input-group">
+                <i class="icon-key-1"></i>
+                <input type="password" name="password" placeholder="Contraseña">
+            </div>
+            <button type="submit" class="btn-login">Entrar</button>
+
+        </form>
+    </div>
 @endsection
