769edda5f9bfefce1227b390b05ce5150f88e80c
Matias Santillan <sntlln.93@gmail.com>
2021-04-08 21:07:39 -0300

    replaced the helper implementation for the model implementation to find an image for the cities

--- resources/views/web/cities.blade.php
+++ resources/views/web/cities.blade.php
@@ -7,7 +7,7 @@
     <div class="box-flex district">
         @foreach($cities as $city)
             <a class="card-flex-district" href="{{ url($district->slug.'/localidades/'.$city->slug) }}">
-                <img src="{{ findAll($city->locations->random()->id, 'App\\Models\\Location')->first() }}" alt="{{ $city->slug }}">
+                <img src="{{ $city->images->count() > 0 ? asset('storage/'.$city->images->random()->path) : asset('img/no-image.png') }}" alt="{{ $city->slug }}">
                 <h3>{{ $city->name }}</h3>
             </a>
         @endforeach
