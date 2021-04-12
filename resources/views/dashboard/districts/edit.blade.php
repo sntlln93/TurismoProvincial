bb44335879101588e5572e593593c782ae77eb31
Matias Santillan <sntlln.93@gmail.com>
2021-04-12 11:00:45 -0300

    added error messages to these forms

--- resources/views/dashboard/districts/edit.blade.php
+++ resources/views/dashboard/districts/edit.blade.php
@@ -1,5 +1,21 @@
 @extends('dashboard.layouts.app')
 
+@section('styles')
+    <style>
+        .error-message {
+            color: red;
+            margin: 0 1em 1em 0;
+            font-weight: 500;
+            text-align: right;
+        }
+
+        .error-input {
+            border-color: red !important;
+        }
+
+    </style>
+@endsection
+
 @section('content')
     <main>
         <div class="title" style="margin-bottom: 2em">
@@ -14,8 +30,8 @@
                 <div class="modal-body">
                     <form action="{{ url('panel-de-administracion/districts/'.$district->id) }}" method="POST">
                         @csrf @method('PUT')
-                        <div><h4>Intendente:</h4><input type="text" name="mayor" value="{{ $district->mayor }}" placeholder=""></div>
-                    
+                        <div><h4>Intendente:</h4><input class="@error('mayor') error-input @enderror" type="text" name="mayor" value="{{ $district->mayor }}" placeholder=""></div>
+                        @error('mayor') <small class="error-message"> {{ $message }} </small> @enderror
                         <button type="submit" class="save">Guardar<i class="icon-floppy"></i>    
                     </form>
                 </div>
