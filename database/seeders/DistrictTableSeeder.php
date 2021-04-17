7b09c82b15cfed47c57393ade35e41edd35d0fd2
Matias Santillan <sntlln.93@gmail.com>
2021-04-16 21:18:47 -0300

    removed-default-district-image

--- database/seeders/DistrictTableSeeder.php
+++ database/seeders/DistrictTableSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 
 use App\Models\District;
-use App\Models\Image;
 
 class DistrictTableSeeder extends Seeder
 {
@@ -16,7 +15,6 @@ class DistrictTableSeeder extends Seeder
      */
     public function run()
     {
-
         $district = District::create([
             'id' => 1,
             'name' => 'Capital',
@@ -25,25 +23,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'capital',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
-
         $district = District::create([
             'id' => 2,
             'name' => 'Gral. San Martín',
             'mayor' => 'Rodolfo Nicolás Flores',
             'description' => 'General San Martín es un departamento ubicado al sur de la provincia de La Rioja (Argentina). Su Cabecera departamental es el poblado de Ulapes.',
             'slug' => 'gral-san-martin',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -54,24 +39,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'arauco',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 4,
             'name' => 'Castro Barros',
             'mayor' => 'Marcelo Del Moral',
             'description' => 'Este departamento es popularmente conocido como "la costa riojana". Este apelativo se debe a que las localidades se ubican sobre el "costado" oriental del cordón de Velasco. La vía terrestre más importante de acceso es la ruta nacional N° 75, que une la ciudad de La Rioja con la localidad de Aimogasta. Su nombre honra la memoria del sacerdote riojano Pedro Ignacio de Castro Barros, miembro del cuarto período de la Asamblea del Año XIII y del Congreso de Tucumán.',
             'slug' => 'castro-barros',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -82,24 +55,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'chamical',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 6,
             'name' => 'Chilecito',
             'mayor' => 'Rodrigo Brizuela y Doria',
             'description' => 'Chilecito es un departamento ubicado en la provincia de La Rioja (Argentina). La cabecera departamental es la ciudad de Chilecito. El departamento comprende la ciudad de Chilecito y los distritos: Anguinán, Guanchín, La Puntilla, Los Sarmientos, Malligasta, Miranda, Nonogasta, San Miguel, San Nicolás, Santa Florentina, Sañogasta, Tilimuqui, y Vichigasta.',
             'slug' => 'chilecito',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -110,24 +71,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'gral-felipe-varela',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 8,
             'name' => 'Famatina',
             'mayor' => 'Alberto Godoy',
             'description' => 'Famatina es un departamento ubicado en la provincia de La Rioja (Argentina). Su ciudad cabecera, la localidad de Famatina, se encuentra a una distancia de 230 km de la ciudad de La Rioja y a 1234 km de la ciudad de Buenos Aires. La palabra "Famatina" derivaría de la voz quechua "Huamatinag" o posiblemente "wamatinag" que significa "madre de los metales".',
             'slug' => 'famatina',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -138,24 +87,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'gral-angel-vicente-peñaloza',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 10,
             'name' => 'Gral. Belgrano',
             'mayor' => 'Carlos Alberto Romero',
             'description' => 'General Belgrano es uno de los 18 departamentos en los que se divide la provincia de La Rioja (Argentina). Se encuentra el la región sudeste de la provincia, en el área llamada "De los LLanos" por sus características topográficas. Olta, la ciudad cabecera del departamento es llamada "Jardín de los Llanos". Su nombre honra a Manuel Belgrano, General del Ejército del Norte.',
             'slug' => 'gral-belgrano',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -167,24 +104,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'gral-juan-facundo-quiroga',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 12,
             'name' => 'Gral. Lamadrid',
             'mayor' => 'Daniela Mabel López Roudier',
             'description' => 'General Lamadrid es uno de los 18 departamentos en los que se divide la provincia de La Rioja (Argentina). La cabecera del departamento es la localidad de Villa Castelli, donde se concentra casi la totalidad de la población. Se encuentra a 330 km de la Ciudad de La Rioja y a 1315 km de la Ciudad de Buenos Aires- El principal acceso lo constituye la RN 76. El departamento cuenta con dos escuelas de nivel inicial y una escuela de nivel medio de especialización agrotécnica, centro de atención promaria en salud, servicios públicos y comunicaciones, centralizados en la localidad cabecera.',
             'slug' => 'gral-lamadrid',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -195,24 +120,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'gral-ortiz-de-ocampo',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 14,
             'name' => 'Independencia',
             'mayor' => 'Juan Gonzalo Herrera',
             'description' => 'El departamento fue creado por la ley 108 del 28 de septiembre de 1864 (156 años), norma que también fijó como localidad cabecera a Patquía.',
             'slug' => 'independencia',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -223,24 +136,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'san-blas-de-los-sauces',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 16,
             'name' => 'Rosario Vera Peñaloza',
             'mayor' => 'Cristián Eduardo Perez',
             'description' => 'Rosario Vera Peñaloza es un departamento ubicado en la provincia de La Rioja (Argentina). El Departamento Rosario Vera Peñaloza, es el único en la provincia que lleva nombre de mujer, en homenaje a la llamada Maestra de la Patria, que le fue impuesto por Ley 2890, del 28 de julio de 1964 (56 años)',
             'slug' => 'rosario-vera-peñaloza',
-        ]);
-
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
 
         $district = District::create([
@@ -251,24 +152,12 @@ class DistrictTableSeeder extends Seeder
             'slug' => 'sanagasta',
         ]);
 
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
-        ]);
-
         $district = District::create([
             'id' => 18,
             'name' => 'Vinchina',
             'mayor' => 'Gustavo Arias',
             'description' => 'Existen varias versiones, atribuyéndose a palabras quechuas, o de la lengua cacán. Según una versión, el nombre Vinchina derivaría de la combinación de las expresiones "yichi" (labrar la tierra, y el sustantivo derivado lugar labrado) y "anah" (alto, arriba, altura) lo que haría referencia a "lugar labrado en altura". Otra explicación hace referencia a la expresión cacán que significa "lugar con algarrobos". Finalmente, un autor nacido en el departamento indica que el nombre Vinchina surge de la unión de las palabras "vil" (lugar), "chiri" (frío) y "na" (verbo hacer), lo que equivaldría a "lugar donde hace frío".',
             'slug' => 'vinchina',
-        ]);
-        
-        Image::create([
-            'path' => 'img/districts/web/'.$district->slug.'.jpg',
-            'imageable_id' => $district->id,
-            'imageable_type' => 'App\\Models\\District',
         ]);
     }
 }
