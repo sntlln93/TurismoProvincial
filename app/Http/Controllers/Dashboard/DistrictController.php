ff84ea6126a1e313f1ae689db9a1d87c6f4fa2dc
Matias Santillan <sntlln.93@gmail.com>
2021-04-19 12:25:16 -0300

    se agregó el campo photo a district.edit para que el usuario municipal pueda modificar la imagen

--- app/Http/Controllers/Dashboard/DistrictController.php
+++ app/Http/Controllers/Dashboard/DistrictController.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers\Dashboard;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
 
 use App\Models\District;
+use App\Models\Image;
 
 class DistrictController extends Controller
 {
     public function index()
     {
+        if(Auth::user()->role->name !='Admin Provincia') {
+            return redirect('panel-de-administracion');
+        }
+        
         $districts = District::all();
         
         return view('dashboard.districts.index')->with('districts', $districts);
@@ -35,10 +42,34 @@ class DistrictController extends Controller
         if(!$can_edit){
             return back()->with('error', 'no tienes permiso para realizar esta acción');
         }
-
-        $mayor = $request->validate(['mayor' => 'required', 'description' => 'sometimes']);
-
-        $district->update($mayor);
+        
+        $validated_data = $request->validate([
+            'mayor' => 'required',
+            'description' => 'sometimes',
+            'photo' => 'sometimes|image|mimes:jpg,jpeg',
+        ]);
+        
+        DB::transaction(function () use($district, $validated_data) {
+            
+            $district->update([
+                'mayor' => $validated_data['mayor'],
+                'description' => $validated_data['description'],
+            ]);
+            
+            if(array_key_exists('photo', $validated_data)){
+                if(! $district->image) {
+                    Image::create([
+                        'path' => $validated_data['photo']->store('districts', 'public'),
+                        'imageable_id' => $district->id,
+                        'imageable_type' => 'App\\Models\\District'
+                        ]);
+                    } else {
+                        $image = $district->image;
+                        Storage::delete('storage/'.$image->path);
+                        $image->update(['path' => $validated_data['photo']->store('districts', 'public')]);
+                    }
+                }
+        });
 
         return redirect('panel-de-administracion/districts/');
     }
