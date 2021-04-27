f96d96b615d0a41eabb2f5aefda0e7849ee3c4ca
Matias Santillan <sntlln.93@gmail.com>
2021-04-27 11:00:44 -0300

    fixed broken redirect when logged

--- app/Http/Controllers/Auth/ChangePasswordController.php
+++ app/Http/Controllers/Auth/ChangePasswordController.php
@@ -7,8 +7,6 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
-use App\User;
-
 class ChangePasswordController extends Controller
 {
     public function edit()
