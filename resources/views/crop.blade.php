f5e9af66613f49129bd0bc5c0d27f8504335a666
Matias Santillan <sntlln.93@gmail.com>
2021-06-14 22:38:06 -0300

    crop extends layouts.app

--- resources/views/crop.blade.php
+++ resources/views/crop.blade.php
@@ -1,30 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('dashboard.layouts.app')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="{{ asset('css/imagecrop.min.css') }}">
-</head>
+@section('content')
+<main>
+    <input type="file"><br>
 
-<body>
-    <main>
-        <input type="file"><br>
+    <div id="cropperContainer"></div>
 
-        <div id="cropperContainer"></div>
+    <form action="{{ url('crop') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <input id="cropped_image" type="hidden" name="img" value="">
+        <img src="" alt="" id="croppedImage">
+        <button>submit</button>
+    </form>
+</main>
+@endsection
 
-        <form action="{{ url('crop') }}" method="POST" enctype="multipart/form-data">
-            @csrf
-            <input id="cropped_image" type="hidden" name="img" value="">
-            <img src="" alt="" id="croppedImage">
-            <button>submit</button>
-        </form>
-    </main>
-
-    <script src="{{ asset('js/imagecrop.min.js') }}"></script>
-    <script>
-        const croperContainer =  document.getElementById("cropperContainer");
+@section('scripts')
+<script src="{{ asset('js/imagecrop.min.js') }}"></script>
+<script>
+    const croperContainer =  document.getElementById("cropperContainer");
         const fileInput = document.querySelector('input[type=file]');
         const croppedInput = document.querySelector('#cropped_image');
         const croppedImage = document.querySelector('#croppedImage');
@@ -35,32 +29,26 @@
 
             reader.addEventListener("load", function () {
                 var cropper = new ImageCropper(croperContainer, reader.result, {
-                max_width: 1920,
-                max_height: 1080,
-                fixed_size: true,
-                min_crop_width: 480,
-                update_cb: function (p) {
-                    cropped_image_src = cropper.crop("image/jpeg", 1);
-                    croppedInput.value = cropped_image_src;
-                    croppedImage.src = cropped_image_src;
-                    
-                },
-            });
-                console.log(cropper);
+                    max_width: 1920,
+                    max_height: 1080,
+                    fixed_size: false,
+                    update_cb: function (p) {
+                        cropped_image_src = cropper.crop("image/jpeg", 1);
+                        croppedInput.value = cropped_image_src;
+                        croppedImage.src = cropped_image_src;
+                    },
+                });
             }, false);
 
             if (file) {
                 reader.readAsDataURL(file);
-                console.log(reader);
             }
         }
 
-    fileInput.addEventListener('change', handleCropper);
+        fileInput.addEventListener('change', handleCropper);
+</script>
+@endsection
 
-
-    </script>
-
-
-</body>
-
-</html>
+@section('styles')
+<link rel="stylesheet" href="{{ asset('css/imagecrop.min.css') }}">
+@endsection
