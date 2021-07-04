d6bbacb9b261911d6e4af921f538ff97b19d1ba6
AldanaBaurer <aldana.baurer@sponsorize.ch>
2021-07-04 20:19:11 -0300

    activities styles - web page

--- resources/views/web/activities.blade.php
+++ resources/views/web/activities.blade.php
@@ -3,14 +3,14 @@
 @section('content')
 <main class="main-web">
     <h2>Experiencias</h2>
-    @foreach($activities as $activity)
-        <div class="box-flex activities">
+    <div class="box-flex activities">
+        @foreach($activities as $activity)
             <a class="card-flex-activities" href="{{ url($district->slug.'/experiencias/'.$activity->slug) }}">
                 <img src="{{ findOne($activity->id, 'App\\Models\\Activity')->first() }}" alt="{{ $activity->slug }}">
                 <h3>{{ $activity->name }}</h3>
                 <h5>{{ $activity->start->diffForHumans() }}</h5>
             </a>
-        </div>
-    @endforeach
+        @endforeach
+    </div>
 </main>
 @endsection
