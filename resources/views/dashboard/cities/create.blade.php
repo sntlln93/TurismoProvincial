f7b265418a70f86fc044b8f44f3724797c4bc926
Matias Santillan <sntlln.93@gmail.com>
2021-07-06 11:24:55 -0300

    fixed form markup by removing modal div

--- resources/views/dashboard/cities/create.blade.php
+++ resources/views/dashboard/cities/create.blade.php
@@ -56,42 +56,38 @@
         <h2>Agregar localidad</h2>
     </div>
 
-    <div class="modal-body">
-        <form action="{{ url('panel-de-administracion/cities') }}" method="POST" class="modal-body">
-            @csrf
-            <div>
-                <h4>Nombre:</h4><input class="@error('name') error-input @enderror" type="text" name="name" value=""
-                    placeholder="Nombre de la localidad">
-            </div>
-            @error('name') <small class="error-message">{{ $message }}</small> @enderror
+    <form action="{{ url('panel-de-administracion/cities') }}" method="POST" class="modal-body">
+        @csrf
+        <div>
+            <h4>Nombre:</h4><input class="@error('name') error-input @enderror" type="text" name="name" value=""
+                placeholder="Nombre de la localidad">
+        </div>
+        @error('name') <small class="error-message">{{ $message }}</small> @enderror
 
-            <div>
-                <h4>Descripción:</h4>
-                <textarea class="@error('description') error-input @enderror msj" name="description" maxlength="1000"
-                    rows="5" placeholder="Escribí una descripción de la localidad"></textarea>
-            </div>
-            @error('description') <small class="error-message">{{ $message }}</small> @enderror
-            <label class="cont">Cantidad de carácteres: 0/1000</label>
-
-
-            <div>
-                <h4>Foto:</h4><input class="@error('photo') error-input @enderror" type="file" accept="image/jpeg"
-                    id="photo">
-                <input id="croppedImageInput" type="hidden" name="photo">
-            </div>
-            @error('photo') <small class="error-message">{{ $message }}</small> @enderror
-
-            <div class="img--container">
-                <img id="inputImage">
-                <button id="cropBtn">Recortar</button>
-                <img id="croppedImage">
-            </div>
-
-            <button type="submit" class="save">Guardar<i class="icon-floppy"></i>
-        </form>
+        <div>
+            <h4>Descripción:</h4>
+            <textarea class="@error('description') error-input @enderror msj" name="description" maxlength="1000"
+                rows="5" placeholder="Escribí una descripción de la localidad"></textarea>
+        </div>
+        @error('description') <small class="error-message">{{ $message }}</small> @enderror
+        <label class="cont">Cantidad de carácteres: 0/1000</label>
 
 
-    </div>
+        <div>
+            <h4>Foto:</h4><input class="@error('photo') error-input @enderror" type="file" accept="image/jpeg"
+                id="photo">
+            <input id="croppedImageInput" type="hidden" name="photo">
+        </div>
+        @error('photo') <small class="error-message">{{ $message }}</small> @enderror
+
+        <div class="img--container">
+            <img id="inputImage">
+            <button id="cropBtn">Recortar</button>
+            <img id="croppedImage">
+        </div>
+
+        <button type="submit" class="save">Guardar<i class="icon-floppy"></i>
+    </form>
 </main>
 @endsection
 
