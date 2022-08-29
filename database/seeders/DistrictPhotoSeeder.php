b35ec215c9dc1c35db60c9049f3cdecdcf097da7
Matias Santillan <sntlln.93@gmail.com>
2022-08-29 05:57:26 -0300

    fixing image extension

--- database/seeders/DistrictPhotoSeeder.php
+++ database/seeders/DistrictPhotoSeeder.php
@@ -19,7 +19,7 @@ class DistrictPhotoSeeder extends Seeder
 
         foreach ($districts as $district) {
             Image::create([
-                'path' => 'districts/' . $district->slug . '.jpg',
+                'path' => 'districts/' . $district->slug . '.png',
                 'imageable_id' => $district->id,
                 'imageable_type' => 'App\\Models\\District'
             ]);
