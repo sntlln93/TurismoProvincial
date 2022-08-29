bf4580bbaa4120a55585f15df5669c4071b09f3b
Matias Santillan <sntlln.93@gmail.com>
2022-08-29 04:03:34 -0300

    adding gd extension for images

--- app/Services/StoreImageService.php
+++ app/Services/StoreImageService.php
@@ -10,11 +10,11 @@ class StoreImageService
     /**
      * Crops and saves an image to the main storage
      *
-     * @param string $photo
+     * @param array $photo
      * @param string $relative_path
      * @return string
      */
-    public function store(string $photo, string $relative_path): string
+    public function store(array $photo, string $relative_path): string
     {
         $decoded_image = str_replace('data:image/jpeg;base64,', '', $photo['base64']);
         $decoded_image = str_replace(' ', '+', $decoded_image);
