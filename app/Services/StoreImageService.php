9911b0286e1b16abbdfcad42ce059c191ae4dcbb
Matias Santillan <sntlln.93@gmail.com>
2022-08-29 03:43:22 -0300

    adding gd extension for images

--- app/Services/StoreImageService.php
+++ app/Services/StoreImageService.php
@@ -3,18 +3,24 @@
 namespace App\Services;
 
 use Illuminate\Support\Str;
-use Illuminate\Support\Facades\Storage;
 use Intervention\Image\Facades\Image;
 
 class StoreImageService
 {
-    public function store($photo, $relative_path)
+    /**
+     * Crops and saves an image to the main storage
+     *
+     * @param string $photo
+     * @param string $relative_path
+     * @return string
+     */
+    public function store(string $photo, string $relative_path): string
     {
         $decoded_image = str_replace('data:image/jpeg;base64,', '', $photo['base64']);
         $decoded_image = str_replace(' ', '+', $decoded_image);
 
         $image_name = Str::random(15).'.'.'webp';
-        
+
         $path = $relative_path.'/'.$image_name;
 
         Image::make($decoded_image)->crop(
@@ -22,7 +28,7 @@ class StoreImageService
             intval(ceil($photo['h'])),
             intval(ceil($photo['x'])),
             intval(ceil($photo['y'])))
-        ->save(storage_path('storage/'.$path));
+        ->save(storage_path($path));
 
         return $path;
     }
